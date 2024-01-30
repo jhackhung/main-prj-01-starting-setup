d38b1b660887117d8306fe32448af6f411fd69f1
Jack <hungjack1222@gmail.com>
2024-01-30 20:32:10 +0800

    Feat: Create Pages & Add to Route Config

--- src/router.js
+++ src/router.js
@@ -1,16 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoachesList from './pages/CoachesList.vue';
+import CoachDetail from './pages/CoachDetail.vue';
+import CoachContact from './pages/CoachContact.vue';
+import CoachRegistration from './pages/CoachRegistration.vue';
+import RequestReceived from './pages/RequestReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/coaches' },
-        { path: '/coaches', component: null },
-        { path: '/coaches/:id', component: null, children: [
-            { path: 'contact', component: null } // /coaches/:id/contact
+        { path: '/coaches', component: CoachesList },
+        { path: '/coaches/:id', component: CoachDetail, children: [
+            { path: 'contact', component: CoachContact } // /coaches/:id/contact
         ] },
-        { path: '/register', component: null },
-        { path: '/requests', component: null },
-        { path: '/notFound(/*)', component: null }
+        { path: '/register', component: CoachRegistration },
+        { path: '/requests', component: RequestReceived },
+        { path: '/notFound(/*)', component: NotFound }
     ],
 });
 
