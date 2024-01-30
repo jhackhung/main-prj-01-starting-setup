5525141ac1829e5d5e1477298959e38808e62042
Jack <hungjack1222@gmail.com>
2024-01-30 20:38:39 +0800

    Feat: Create TheHeader.vue

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <h1>
+            <router-link to="/">Find the Coach</router-link>
+        </h1>
+        <nav>
+            <ul>
+                <li>
+                    <router-link to="/coaches">Coaches</router-link>
+                </li>
+                <li>
+                    <router-link to="/requests">Requests</router-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
