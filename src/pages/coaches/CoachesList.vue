dcdc959eff5b2b68288db46595743576dcb92d81
Jack <hungjack1222@gmail.com>
2024-01-31 23:27:29 +0800

    Feat: Render a Loading Spinner Also Update some logic with isLoading

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,9 +6,12 @@
         <base-card>
             <div class="controls">
                 <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
-                <base-button v-if="!isCoach" link to="/register">Register as Coach</base-button>
+                <base-button v-if="!isCoach && !isLoading" link to="/register">Register as Coach</base-button>
             </div>
-            <ul v-if="hasCoaches">
+            <div v-if="isLoading">
+                <base-spinner></base-spinner>
+            </div>
+            <ul v-else-if="hasCoaches">
                 <coach-item v-for="coach in filteredCoaches" :key="coach.id" :id="coach.id" :first-name="coach.firstName"
                     :last-name="coach.lastName" :rate="coach.hourlyRate" :areas="coach.areas"></coach-item>
             </ul>
@@ -27,6 +30,7 @@ export default {
     },
     data() {
         return {
+            isLoading: false,
             activeFilters: {
                 frontend: true,
                 backend: true,
@@ -51,7 +55,7 @@ export default {
             })
         },
         hasCoaches() {
-            return this.$store.getters['coaches/hasCoaches']
+            return !this.isLoading && this.$store.getters['coaches/hasCoaches']
         },
         isCoach() {
             return this.$store.getters['coaches/isCoach'];
@@ -61,8 +65,10 @@ export default {
         setFilters(updatedFilters) {
             this.activeFilters = updatedFilters;
         },
-        loadCoaches() {
-            this.$store.dispatch('coaches/loadCoaches');
+        async loadCoaches() {
+            this.isLoading = true;
+            await this.$store.dispatch('coaches/loadCoaches'); // await is used to wait for the promise to resolve/complete
+            this.isLoading = false;
         }
     },
     created() {
