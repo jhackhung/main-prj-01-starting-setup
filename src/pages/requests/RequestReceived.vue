06790ed0f36328c0ee44e015447d3cad217d565d
Jack <hungjack1222@gmail.com>
2024-02-01 20:52:55 +0800

    Feat: Send Coach Requests with HTTP Request

--- src/pages/requests/RequestReceived.vue
+++ src/pages/requests/RequestReceived.vue
@@ -1,17 +1,19 @@
 <template>
+    <base-dialog :show="!!error" title="An error occured" @close="handleError">
+        <p>{{ error }}</p>
+    </base-dialog>
     <section>
         <base-card>
             <header>
                 <h2>Request Received</h2>
             </header>
+            <base-spinner v-if="isLoading"></base-spinner>
+            <ul v-else-if="hasRequests && !isLoading">
+                <request-item v-for="request in receivedRequests" :key="request.id" :email="request.userEmail"
+                    :message="request.message"></request-item>
+            </ul>
+            <h3 v-else>You haven't received any requests yet!</h3>
         </base-card>
-        <ul v-if="hasRequests">
-            <request-item v-for="request in receivedRequests"
-                          :key="request.id"
-                          :email="request.userEmail"
-                          :message="request.message"></request-item>
-        </ul>
-        <h3 v-else>You haven't received any requests yet!</h3>
     </section>
 </template>
 <script>
@@ -21,12 +23,35 @@ export default {
     components: {
         RequestItem
     },
+    data() {
+        return {
+            isLoading: false,
+            error: null,
+        }
+    },
     computed: {
         receivedRequests() {
             return this.$store.getters['requests/requests'];
         },
         hasRequests() {
             return this.$store.getters['requests/hasRequests'];
+        }
+    },
+    created() {
+        this.loadRequests();
+    },
+    methods: {
+        async loadRequests() {
+            this.isLoading = true;
+            try {
+                await this.$store.dispatch('requests/fetchRequests');
+            } catch (error) {
+                this.error = error.message || 'Something failed!';
+            }
+            this.isLoading = false;
+        },
+        handleError() {
+            this.error = null;
         }
     }
 }
--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,12 +1,50 @@
 export default {
-    contactCoach(context, payload) {
+    async contactCoach(context, payload) {
         const newRequest = {
-            id: new Date().toISOString(),
-            coachId: payload.coachId,
             userEmail: payload.email,
             message: payload.message
         }
 
+        const response = await fetch(`https://vue-main-prj-aead4-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`, {
+            method: 'POST',
+            body: JSON.stringify(newRequest)
+        });
+
+        const responseData = await response.json();
+
+        // console.log(responseData); We can see that the response data has a name property that is the id of the new request.
+
+        if(!response.ok) {
+            const error = new Error(responseData.message || 'Failed to send request.');
+            throw error;
+        }
+
+        newRequest.id = responseData.name;
+        newRequest.coachId = payload.coachId;
+
         context.commit('addRequest', newRequest);
+    },
+    async fetchRequests(context) {
+        const coachId = context.rootGetters.userId; 
+        const response = await fetch(`https://vue-main-prj-aead4-default-rtdb.firebaseio.com/requests/${coachId}.json`)
+        const responseData = await response.json();
+
+        if(!response.ok) {
+            const error = new Error(responseData.message || 'Failed to fetch requests.');
+            throw error;
+        }
+
+        const requests = [];
+        for (const key in responseData) {
+            const request = {
+                id: key,
+                coachId: coachId,
+                userEmail: responseData[key].userEmail,
+                message: responseData[key].message
+            };
+            requests.push(request);
+        }
+
+        context.commit('setRequest', requests);
     }
 }
