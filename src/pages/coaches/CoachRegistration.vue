dff84d42e10aa6e24f5cbfbdc2b382f2af706ced
Jack <hungjack1222@gmail.com>
2024-02-01 11:20:45 +0800

    Feat: Handle error and loading to Coach Registration

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,7 +1,13 @@
 <template>
+    <base-dialog :show="!!error" title="An error occurred!" @close="handleError">
+        <p>{{ error }}</p>
+    </base-dialog>
     <section>
         <base-card>
             <h2>Register as a coach now!</h2>
+            <div v-if="isLoading">
+                <base-spinner></base-spinner>
+            </div>
             <coach-form @save-data="saveData"></coach-form>
         </base-card>
     </section>
@@ -14,10 +20,27 @@ export default {
     components: {
         CoachForm,
     },
+    data() {
+        return {
+            isLoading: false,
+            error: null,
+        }
+    },
     methods: {
-        saveData(formData) {
-            this.$store.dispatch('coaches/registerCoach', formData);
-            this.$router.replace('/coaches'); // redirect to coaches page, and you can't go back to this page
+        async saveData(formData) {
+            this.isLoading = true;
+            try {
+                await this.$store.dispatch('coaches/registerCoach', formData);
+            } catch (error) {
+                this.error = error.message || 'Failed to register coach.';
+            }
+            if (!this.error) {
+                this.$router.replace('/coaches'); // redirect to coaches page, and you can't go back to this page
+            }
+            this.isLoading = false;
+        },
+        handleError() {
+            this.error = null;
         }
     }
 
