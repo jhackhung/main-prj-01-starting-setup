ee11460349ee7ebd8c1937754074e9c4dadc87c4
Jack <hungjack1222@gmail.com>
2024-03-10 23:20:00 +0800

    Feat: Create Login
    Create action of login and dispatch in UserAuth
    
    The only change is the url of fetching login api.
    
    Add auth query to fetching for request and register coach
    
    Control some button by isLoggedIn. Like in the Header and coach list page. Control different buttons.

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,58 +1,66 @@
 export default {
-    async registerCoach(context, data) {
-        const userId = context.rootGetters.userId;
-        const coachData = {
-            firstName: data.first,
-            lastName: data.last,
-            description: data.desc,
-            hourlyRate: data.rate,
-            areas: data.areas
-        };
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
+    const coachData = {
+      firstName: data.first,
+      lastName: data.last,
+      description: data.desc,
+      hourlyRate: data.rate,
+      areas: data.areas,
+    };
 
-        const response = await fetch(`https://vue-main-prj-aead4-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
-            method: 'PUT', // Data is overwritten if it already exists or will be created if it doesn't exist
-            body: JSON.stringify(coachData),
-        });
+    const token = context.rootGetters.token;
 
-        const responseData = await response.json();
+    const response = await fetch(
+      `https://vue-main-prj-aead4-default-rtdb.firebaseio.com/coaches/${userId}.json?auth=` +
+        token,
+      {
+        method: 'PUT', // Data is overwritten if it already exists or will be created if it doesn't exist
+        body: JSON.stringify(coachData),
+      }
+    );
 
-        if (!response.ok) {
-            const error = new Error(responseData.message || 'Failed to fetch!');
-            throw error;
-        }
-        context.commit('registerCoach', {
-            ...coachData,
-            id: userId
-        });
-    },
-    async loadCoaches(context, payload) {
-        if(!payload.forceRefresh && !context.getters.shouldUpdate) {
-            return;
-        }
+    const responseData = await response.json();
 
-        const response = await fetch(`https://vue-main-prj-aead4-default-rtdb.firebaseio.com/coaches.json`);
-        const responseData = await response.json();
-
-        if(!response.ok) {
-            const error = new Error(responseData.message || 'Failed to fetch!');
-            throw error;
-        }
-
-        const coaches = [];
-
-        for (const key in responseData) {
-            const coach = {
-                id: key,
-                firstName: responseData[key].firstName,
-                lastName: responseData[key].lastName,
-                description: responseData[key].description,
-                hourlyRate: responseData[key].hourlyRate,
-                areas: responseData[key].areas
-            };
-            coaches.push(coach);
-        }
-        
-        context.commit('setCoaches', coaches);
-        context.commit('setFetchTimestamp');
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to fetch!');
+      throw error;
     }
-}
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId,
+    });
+  },
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
+
+    const response = await fetch(
+      `https://vue-main-prj-aead4-default-rtdb.firebaseio.com/coaches.json`
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to fetch!');
+      throw error;
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp');
+  },
+};
